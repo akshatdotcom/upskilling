56cc09bc724e7765f633477a4cce5b001f1c124c
Akshat Shah <aksshah2004@gmail.com>
2023-12-03 01:51:13 -0600

    add link to home page for logo

--- website/src/components/Navbar/Title.js
+++ website/src/components/Navbar/Title.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
 import logo from './logo.svg'
 import './Navbar.css'
+import { Link } from 'react-router-dom'
 
 class Title extends Component {
 
     render() {
         return (
-            <img className="logo" src={logo} alt="Pathlete"></img>
+            <Link to="/">
+                <img className="logo" src={logo} alt="Pathlete"></img>
+            </Link>
         )
     }
 }
