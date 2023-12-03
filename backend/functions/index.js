0d977d4b9d42afd411c1d181e9552a5809a0a198
Akshat Shah <aksshah2004@gmail.com>
2023-12-03 06:03:21 -0600

    implement chatgpt api call

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -7,13 +7,45 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const axios = require("axios");
+const {onCall} = require("firebase-functions/v2/https");
 
-exports.helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+
+admin.initializeApp();
+
+const apiKey = process.env.OPENAI_API_KEY;
+
+exports.getOpenAIResponse = onCall(async (req) => {
+  try {
+    const conversation = req.data.conversation;
+
+    if (!conversation) {
+      throw new functions.https.HttpsError("invalid-arg", "API Error");
+    }
+
+    const url = "https://api.openai.com/v1/chat/completions";
+
+    const headers = {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${apiKey}`,
+    };
+
+    const body = {
+      messages: conversation,
+      model: "gpt-3.5-turbo",
+    };
+
+    const response = await axios.post(url, body, {headers});
+
+    if (response.status === 200) {
+      return {response: response.data.choices[0].message.content};
+    } else {
+      throw new functions.https.HttpsError("internal", "Request failed.");
+    }
+  } catch (error) {
+    throw new functions.https.HttpsError("internal", error.message);
+  }
 });
