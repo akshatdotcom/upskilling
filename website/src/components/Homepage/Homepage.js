6598daed20adb514ac1dea2e0207e8f0d0c5737e
Akshat Shah <aksshah2004@gmail.com>
2023-12-03 01:43:00 -0600

    format homepage code

--- website/src/components/Homepage/Homepage.js
+++ website/src/components/Homepage/Homepage.js
@@ -10,27 +10,27 @@ class Homepage extends Component {
     render() {
         return (
             <div className="homepage-screen">
-            <h1 className="homepage-title">
-                Discover Your New Future
-                <img className="runner-icon" src={runner} alt="Runner"/>
-            </h1>
-            <main className="homepage-header">
-                <p> Explore possible career paths within athletics <br></br>using your acheived education.
-                </p>
-            </main>
-            <div>
-            <img className="student-icon" src={student} alt="Student"/>
-            </div>
-            <div>
-            <QuizButton>Take the Quiz</QuizButton>
-            </div>
-            <div>
-            <img className="footballer-icon" src={footballer} alt="Footballer"/>
-            </div>
+                <h1 className="homepage-title">
+                    Discover Your New Future
+                    <img className="runner-icon" src={runner} alt="Runner" />
+                </h1>
+                <main className="homepage-header">
+                    <p> Explore possible career paths within athletics <br></br>using your acheived education.
+                    </p>
+                </main>
+                <div>
+                    <img className="student-icon" src={student} alt="Student" />
+                </div>
+                <div>
+                    <QuizButton>Take the Quiz</QuizButton>
+                </div>
+                <div>
+                    <img className="footballer-icon" src={footballer} alt="Footballer" />
+                </div>
             </div>
         )
     }
-    
+
 }
 
 export default Homepage
