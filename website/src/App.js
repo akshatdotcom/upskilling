111394bb51d47c4a822f1c357d84b2451dd7e28b
Akshat Shah <aksshah2004@gmail.com>
2023-12-03 20:40:23 -0600

    remove extraneous package-lock.json in root dir

--- website/src/App.js
+++ website/src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar/Navbar';
 import CareerQuiz from './components/Quiz/CareerQuiz';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import QuizResults from './components/QuizResults/QuizResults';
+import LessonPlan from './components/LessonPlan/LessonPlan';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           <Route path='/' exact element={<Homepage />} />
           <Route path='/quiz' element={<CareerQuiz />} />
           <Route path='/results' element={<QuizResults />} />
+          <Route path='/lesson-plan' element={<LessonPlan />} />
         </Routes>
       </div>
     </Router>
