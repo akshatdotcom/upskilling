3e06407f978271640723cb94f2ece07a77bd430f
SamuelJacob146 <samuel.t.jacob888@gmail.com>
2023-12-03 20:32:06 -0600

    added updated quiz

--- website/src/components/Quiz/SurveyComponent.jsx
+++ website/src/components/Quiz/SurveyComponent.jsx
@@ -3,7 +3,6 @@ import { Model } from "survey-core";
 import { Survey } from "survey-react-ui";
 import "survey-core/defaultV2.min.css";
 import { themeJson } from "./theme";
-import "./index.css";
 import { json } from "./json";
 
 function SurveyComponent() {
--- website/src/components/Quiz/json.js
+++ website/src/components/Quiz/json.js
@@ -1,29 +1,16 @@
 export const json = {
-    "title": "Athelite Dup",
-    "description": "This is a test. 10/23/2023.",
+    "title": "Career Matching Survey",
+    "description": "Please fill out the questions to find your job matches",
     "logoPosition": "right",
     "pages": [
      {
       "name": "page1",
       "elements": [
        {
-        "type": "dropdown",
+        "type": "text",
         "name": "question1",
         "title": "What is your major?",
-        "choices": [
-         {
-          "value": "Item 1",
-          "text": "Computer Science"
-         },
-         {
-          "value": "Item 2",
-          "text": "Biology"
-         },
-         {
-          "value": "Item 3",
-          "text": "Business"
-         }
-        ]
+        "isRequired": true
        }
       ],
       "title": "Major"
@@ -32,12 +19,71 @@ export const json = {
       "name": "page2",
       "elements": [
        {
-        "type": "rating",
+        "type": "comment",
         "name": "question2",
-        "title": "Choose careers that interest you."
+        "title": "What are some of your interests?",
+        "description": "Separate each interest with a comma",
+        "isRequired": true
        }
       ],
-      "title": "Careers"
+      "title": "Interests"
+     },
+     {
+      "name": "page3",
+      "elements": [
+       {
+        "type": "comment",
+        "name": "question3",
+        "title": "What are some of your academic hobbies?",
+        "description": "Separate each interest with a comma",
+        "isRequired": true
+       }
+      ],
+      "title": "Hobbies"
+     },
+     {
+      "name": "page4",
+      "elements": [
+       {
+        "type": "rating",
+        "name": "question4",
+        "title": "On a scale of 1-5, how social are you?",
+        "description": "1 - Not Social, 5 - Very Social",
+        "isRequired": true
+       }
+      ],
+      "title": "People Skills"
+     },
+     {
+      "name": "page6",
+      "elements": [
+       {
+        "type": "radiogroup",
+        "name": "question5",
+        "title": "How long do you want to spend learning?",
+        "isRequired": true,
+        "choices": [
+         {
+          "value": "Item 1",
+          "text": "Less than 1 month"
+         },
+         {
+          "value": "Item 2",
+          "text": "Between 1-3 months"
+         },
+         {
+          "value": "Item 3",
+          "text": "Between 3-6 months"
+         },
+         {
+          "value": "Item 4",
+          "text": "More than 6 months"
+         }
+        ]
+       }
+      ],
+      "title": "Timeline"
      }
-    ]
+    ],
+    "headerView": "advanced"
    }
