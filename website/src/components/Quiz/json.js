825f6fd559b7ad40e5e48a34213c5164dd0b1818
SamuelJacob146 <samuel.t.jacob888@gmail.com>
2023-12-03 21:27:43 -0600

    updated json.js

--- website/src/components/Quiz/json.js
+++ website/src/components/Quiz/json.js
@@ -8,7 +8,7 @@ export const json = {
       "elements": [
        {
         "type": "text",
-        "name": "question1",
+        "name": "major",
         "title": "What is your major?",
         "isRequired": true
        }
@@ -20,7 +20,7 @@ export const json = {
       "elements": [
        {
         "type": "comment",
-        "name": "question2",
+        "name": "interests",
         "title": "What are some of your interests?",
         "description": "Separate each interest with a comma",
         "isRequired": true
@@ -33,7 +33,7 @@ export const json = {
       "elements": [
        {
         "type": "comment",
-        "name": "question3",
+        "name": "hobbies",
         "title": "What are some of your academic hobbies?",
         "description": "Separate each interest with a comma",
         "isRequired": true
@@ -46,7 +46,7 @@ export const json = {
       "elements": [
        {
         "type": "rating",
-        "name": "question4",
+        "name": "social-skills",
         "title": "On a scale of 1-5, how social are you?",
         "description": "1 - Not Social, 5 - Very Social",
         "isRequired": true
@@ -59,7 +59,7 @@ export const json = {
       "elements": [
        {
         "type": "radiogroup",
-        "name": "question5",
+        "name": "timeline",
         "title": "How long do you want to spend learning?",
         "isRequired": true,
         "choices": [
