f7c7cd770d82af99dbc7e2d905d6106f104a117e
Akshat Shah <aksshah2004@gmail.com>
2023-12-05 20:33:21 -0600

    fix lesson plan below navbar positioning

--- website/src/components/LessonPlan/LessonPlan.js
+++ website/src/components/LessonPlan/LessonPlan.js
@@ -20,14 +20,14 @@ function render(userInfoData) {
       <div style={{ width: 279, height: 54, left: 496, top: 129.01, position: 'absolute', textAlign: 'center', color: '#CC5500', fontSize: 40, fontFamily: 'Ubuntu', fontWeight: '500', wordWrap: 'break-word' }}>My Courses</div>
       <div style={{ width: 494, height: 32, left: 526, top: 183.01, position: 'absolute', color: '#DE7C3D', fontSize: 30, fontFamily: 'Ubuntu', fontWeight: '400', wordWrap: 'break-word' }}>Sports Analytics</div>
       <div style={{ width: 1512, height: 100, left: 0, top: 0, position: 'absolute', background: '#FFE3CF' }} />
-      <div style={{ width: 310, height: 44, left: 194, top: 20, position: 'absolute', color: 'black', fontSize: 30, fontFamily: 'Ubuntu', fontWeight: '400', wordWrap: 'break-word' }}>
+      <div style={{ width: 310, height: 44, left: 140, top: 19, position: 'absolute', color: 'black', fontSize: 30, fontFamily: 'Ubuntu', fontWeight: '400', wordWrap: 'break-word' }}>
         {userInfoData.firstName} {userInfoData.lastName}
       </div>
-      <div style={{ width: 279, height: 19, left: 193, top: 60, position: 'absolute', color: 'black', fontSize: 25, fontFamily: 'Ubuntu', fontWeight: '400', wordWrap: 'break-word' }}>
+      <div style={{ width: 279, height: 19, left: 140, top: 52, position: 'absolute', color: 'black', fontSize: 25, fontFamily: 'Ubuntu', fontWeight: '400', wordWrap: 'break-word' }}>
         {userInfoData.major}
       </div>
-      <div style={{ width: 206, height: 50, left: 1261, top: 35, position: 'absolute', background: '#CC5500' }} />
-      <div style={{ width: 180, height: 33, left: 1274, top: 47, right: 100, position: 'absolute', textAlign: 'center', color: '#FFFFFF', fontSize: 20, fontFamily: 'Ubuntu', fontWeight: '400', wordWrap: 'break-word' }}>Explore More!</div>
+      <div style={{ width: 206, height: 50, left: 1261, top: 27, position: 'absolute', background: '#CC5500' }} />
+      <div style={{ width: 180, height: 33, left: 1274, top: 40, right: 100, position: 'absolute', textAlign: 'center', color: '#FFFFFF', fontSize: 20, fontFamily: 'Ubuntu', fontWeight: '400', wordWrap: 'break-word' }}>Explore More!</div>
       <a href="https://www.coursera.org/learn/foundations-sports-analytics?specialization=sports-analytics"><div className='course-content1'>Lesson 1: Foundations of Sports Analytics</div></a>
       <a href='https://www.coursera.org/learn/machine-learning-sports-analytics?irclickid=0MJwkuRQOx[…]rtners&utm_source=impact&utm_campaign=259799&utm_content=b2c'><div className='course-content2'>Lesson 4: Intro to Machine Learning in Sports Analytics</div></a>
       <a href='https://www.coursera.org/learn/prediction-models-sports-data?specialization=sports-analytics'><div className='course-content3'>Lesson 2: Prediction Models with Sports Data</div></a>
